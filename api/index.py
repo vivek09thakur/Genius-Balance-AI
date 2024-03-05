2c8cb5b7de49a4d02953282d8b66910c185aabec
Vivek Thakur <20509.vivek@gpranchi.org>
2024-03-05 16:59:15 +0530

    commit

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
 from flask import Flask,request,jsonify
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -25,4 +27,11 @@ def loader():
         return jsonify({'data':data})
     except Exception as e:
         return jsonify({'error':str(e)})
+    
+    
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS')
+    return response
 
