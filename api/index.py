e1ba8c57057585f27bfca34bd35e543249293ea9
Vivek Thakur <20509.vivek@gpranchi.org>
2024-03-05 15:39:57 +0530

    commit

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -6,9 +7,22 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route('/greet', methods=['POST'])
-def greet():
-    data = request.get_json()
-    name = data['name']
-    return jsonify({'message': f'Hello, {name}!'})
+@app.loader('/loader',methods=['POST'])
+def loader():
+    if 'csv_data' not in request.files:
+        return jsonify({'error':'No file part'})
+    
+    csv_file = request.files['csv_data']
+    if csv_file.filename == '':
+        return jsonify({'error':'No selected file'})
+    
+    if not csv_file.filename.endswith('.csv'):
+        return jsonify({'error':'Invalid file type, file must be in csv format'})
+    
+    try:
+        df = pd.read_csv(csv_file)
+        data = df.to_dict(orient='records')
+        return jsonify({'data':data})
+    except Exception as e:
+        return jsonify({'error':str(e)})
 
